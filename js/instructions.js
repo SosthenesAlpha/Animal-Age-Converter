319c73b8abe23392ce6f5bb0249a007d51c8956a
TALABI OLABODE FOLORUNSO <30889918+SosthenesAlpha@users.noreply.github.com>
2022-10-28 14:06:04 +0100

    Add files via upload

---
+++ js/instructions.js
@@ -0,0 +1,4 @@
+document.addEventListener("DOMContentLoaded", function(){
+let abishai = document.querySelectorAll('.carousel.carousel-slider');
+M.Carousel.init(abishai,{fullWidth: true, indicators: true});
+});
